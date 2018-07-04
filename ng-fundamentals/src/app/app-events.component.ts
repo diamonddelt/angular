d038ab2fd156e238990401c9140c7d2b9209e7ba
diamonddelt <rrrasti@yahoo.com>
2018-07-04 17:00:15 -0500

    Add static images and modified app template

--- ng-fundamentals/src/app/app-events.component.ts
+++ ng-fundamentals/src/app/app-events.component.ts
@@ -2,7 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-events',
-  template: '<h2>Hello World</h2>'
+  template: `
+  <section class='section'>
+    <div class='container'>
+      <h1>Diamond Delt</h1>
+      <img src="/assets/images/diamond.png"/>
+    </div>
+  </section>
+  `
 })
 export class AppEventsComponent {
   title = 'app';
---
+++ ng-fundamentals/src/styles.js
@@ -0,0 +1,8 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  // You can add global styles to this file, and also import other style files
+  'body': {
+    'backgroundColor': 'hsla(212, 84%, 76%, 0.2)'
+  }
+});
