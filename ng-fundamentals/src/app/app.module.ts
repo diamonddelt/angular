dc252f0981013b5715dd7af72f9c7a09665c5e9f
diamonddelt <rrrasti@yahoo.com>
2018-07-07 10:32:52 -0500

    Add event-thumbnail component and examine @Input decorator

--- ng-fundamentals/src/app/app.module.ts
+++ ng-fundamentals/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppEventsComponent } from './app-events.component';
 import { EventsListComponent } from './events/events-list.component';
+import { EventThumbnailComponent } from './events/event-thumbnail/event-thumbnail.component';
 
 @NgModule({
   declarations: [
     AppEventsComponent,
-    EventsListComponent
+    EventsListComponent,
+    EventThumbnailComponent
   ],
   imports: [
     BrowserModule
--- ng-fundamentals/src/app/events/events-list.component.ts
+++ ng-fundamentals/src/app/events/events-list.component.ts
@@ -2,33 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-events-list',
-    template: `
-    <div class='box'>
-        <h1 class='title is-1'>Upcoming Concerts</h1>
-        <hr>
-        <div class='notification is-success'> {{ event.name }} </div>
-        <table class='table is-fullwidth'>
-            <thead>
-                <th>Date</th>
-                <th>Time</th>
-                <th>Price</th>
-                <th>Genre(s)</th>
-            </thead>
-            <tbody>
-                <tr>
-                    <td> {{ event.date }} </td>
-                    <td> {{ event.time }} </td>
-                    <td> \${{ event.price }} </td>
-                    <td> {{ event.genres }} </td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-    `
+    templateUrl: './events-list.component.html'
 })
 
 export class EventsListComponent {
-    event = {
+    event1 = {
         id: 1,
         name: 'Big Techno Tour',
         date: '3/8/2011',
